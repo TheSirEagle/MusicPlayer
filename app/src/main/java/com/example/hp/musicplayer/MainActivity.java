22a64f4e8fdbdd58a66f3476f14171136805f604
Erikas <erikas.rask@gmail.com>
2018-08-08 22:49:55 +0300

    First commit

---
+++ app/src/main/java/com/example/hp/musicplayer/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.hp.musicplayer;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    List<String> list;
+
+    ListAdapter adapter;
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.ListView);
+
+        list = new ArrayList<>();
+
+        Field[] fields = R.raw.class.getFields();
+        for (int i = 0; i < fields.length; i++) {
+            list.add(fields[i].getName());
+        }
+
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
+
+                int resID = getResources().getIdentifier(list.get(i), "raw", getPackageName());
+                mediaPlayer = MediaPlayer.create(MainActivity.this, resID);
+                mediaPlayer.start();
+            }
+        });
+
+
+    }
+}
+
+

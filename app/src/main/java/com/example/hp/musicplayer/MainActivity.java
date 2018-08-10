f28be9e64d55c73be7f14ff4e43344bafda0cf20
Erikas <erikas.rask@gmail.com>
2018-08-10 10:55:14 +0300

    First commit

--- app/src/main/java/com/example/hp/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/hp/musicplayer/MainActivity.java
@@ -1,66 +1,90 @@
 package com.example.hp.musicplayer;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
+    private static final int MY_PERMISSION_REQUEST = 1;
 
-    List<String> list;
+    ArrayList<String> arrayList;
 
-    ListAdapter adapter;
+    List listView;
 
-    MediaPlayer mediaPlayer;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView) findViewById(R.id.ListView);
 
-        list = new ArrayList<>();
-
-        Field[] fields = R.raw.class.getFields();
-        for (int i = 0; i < fields.length; i++) {
-            list.add(fields[i].getName());
+        Uri myUri = Uri.parse("/storage/sdcard0/music/song.mp3");
+        final MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(getApplicationContext(), myUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
+    final Button play = findViewById(R.id.play);
+    final Button pause = findViewById(R.id.pause);
 
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (mediaPlayer != null) {
-                    mediaPlayer.release();
-                }
-
-                int resID = getResources().getIdentifier(list.get(i), "raw", getPackageName());
-                mediaPlayer = MediaPlayer.create(MainActivity.this, resID);
+            public void onClick(View view) {
                 mediaPlayer.start();
+            }
+
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
             }
         });
 
 
-    }
+
+}
 }
 
 
